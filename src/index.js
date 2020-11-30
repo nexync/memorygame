2829565b6f67f7e88d54aab308ec4f8e8c723a11
Jeff <jeffrey.cheng132@gmail.com>
2020-11-30 00:59:22 -0500

    partial working game

--- src/index.js
+++ src/index.js
@@ -19,12 +19,41 @@ class Board extends React.Component {
         this.state = {
             cardvalues: Array(12).fill(0).map(function(k,ind){return Math.floor(ind/2)}),
             cards: Array(12).fill(false),
+            player1Turn: true,
+            chosen: Array(2).fill(-1),
+            scores: Array(2).fill(0),
         };
     }
     handleClick(i) {
         const cards = this.state.cards.slice();
-        cards[i] = !cards[i];
-        this.setState({cards: cards});
+        const curchosen = this.state.chosen.slice();
+        const newscores = this.state.scores.slice();
+        if (curchosen[0] === -1) {
+            curchosen[0] = i
+            cards[i] = !cards[i];
+            this.setState({cards:cards, chosen: curchosen});
+        } 
+        else if (curchosen[1] === -1){
+            curchosen[1] = i
+            cards[i] = !cards[i]
+            this.setState({cards: cards, chosen: curchosen});  //show cards
+            //alert(curchosen)
+            if (this.state.cardvalues[curchosen[0]] === this.state.cardvalues[curchosen[1]]) {
+                if (this.state.player1Turn) {
+                    newscores[0] = newscores[0]+1
+                } else {
+                    newscores[1] = newscores[1]+1
+                } 
+                this.setState({chosen: Array(2).fill(-1),scores: newscores});
+            } else {
+                const updatecards = cards.slice();
+                for(var k = 0; k < curchosen.length; k++) {
+                    updatecards[curchosen[k]] = !updatecards[curchosen[k]];  //reflip cards
+                }
+                this.setState({chosen: Array(2).fill(-1), player1Turn: !this.state.player1Turn});
+                setTimeout(() => this.setState({cards: updatecards}),1000);  //make cards go away 
+            };
+        }
     }
     renderCard(i,f) {
         return ( <Card 
@@ -35,9 +64,9 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Player 1'
-        const score1 = 'Player 1 Score: ' + 1
-        const score2 = 'Player 2 Score: ' + 2
+        const status = this.state.player1Turn ? 'Player 1' : 'Player 2';
+        const score1 = 'Player 1 Score: ' + this.state.scores[0];
+        const score2 = 'Player 2 Score: ' + this.state.scores[1];
         return (
             <div>
                 <div className = "status">{status}</div>
